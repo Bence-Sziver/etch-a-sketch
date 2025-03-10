31aec75b8308902995f56abd0a8c4ee9c2830099
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2025-03-10 15:01:49 +0100

    Create 16x16 grid

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector("#container");
+
+createGrid();
+
+function createGrid() {
+  for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    for (let j = 0; j < 16; j++) {
+      const item = document.createElement("div");
+      item.classList.add("item");
+      row.appendChild(item);
+    }
+  }
+}
