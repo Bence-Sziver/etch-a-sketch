6ce6ed412a13521c5adb38f09e35ede2cff4cc3d
Bence Sziver <105170450+Bence-Sziver@users.noreply.github.com>
2025-03-10 15:36:32 +0100

    Remove and generate new grids

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 const container = document.querySelector("#container");
-
-createGrid();
+const btn = document.querySelector("button");
+let rows = createGrid();
 
 container.addEventListener("mouseover", event => {
   event.target.classList.add("hover");
 });
 
-function createGrid() {
-  for (let i = 0; i < 16; i++) {
+btn.addEventListener("click", () => {
+  let size = +prompt("Add a size for the board (min: 1, max 100): ", "16");
+  if (!size) return alert("Wrong input");
+  if (size > 100) size = 100;
+  if (size < 1) size = 1;
+  removeGrid(rows);
+  rows = createGrid(size);
+});
+
+function createGrid(size = 16) {
+  for (let i = 0; i < size; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
     container.appendChild(row);
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < size; j++) {
       const item = document.createElement("div");
       item.classList.add("item");
       row.appendChild(item);
     }
   }
+  return document.querySelectorAll(".row");
+}
+
+function removeGrid(nodeList) {
+  nodeList.forEach(node => {
+    container.removeChild(node);
+  });
 }
